76ac23821e2a3c21f83def984152c9f37ec35e5f
joerdixon <joseph35dixon@gmail.com>
2023-02-09 19:07:24 -0800

    vibe added

---
+++ commands/vibe.js
@@ -0,0 +1,10 @@
+const {SlashCommandBuilder} = require("discord.js")
+
+module.exports = {
+    data: new SlashCommandBuilder()
+    .setName("vibe")
+    .setDescription("Set the mood"),
+    async execute(interaction) {
+        await interaction.reply("stfu")
+    }
+}
