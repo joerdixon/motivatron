5970dbe5d9c770101914db5b2294f111bea16187
joerdixon <joseph35dixon@gmail.com>
2023-02-12 19:05:23 -0800

    data description

--- commands/data.js
+++ commands/data.js
@@ -1,5 +1,7 @@
 const {SlashCommandBuilder} = require("discord.js");
 
+// The data command will show the user extensive information about their server profile.
+
 module.exports = {
     data: new SlashCommandBuilder()
     .setName("data")
